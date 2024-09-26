a6bdf8b58a5d0ba1502de7d2f83308e7029b7f8f
Shéol <cedric-bouquet-7@outlook.fr>
2024-09-26 18:41:16 +0200

    :memo: Update projet

---
+++ model.js
@@ -0,0 +1,626 @@
+const model = [
+{
+	dresseur: 'Ludwig',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Cryobra', 'level': 48, 'types': ['Glace', 'Poison']},
+				{'name': 'Werdoba', 'level': 50, 'types': ['Feu', 'Glace']},
+				{'name': 'Oursberg', 'level': 48, 'types': ['Eau', 'Glace']},
+				{'name': 'Escarlace', 'level': 46, 'types': ['Glace', 'Terre']},
+				{'name': 'Cristargot', 'level': 50, 'types': ['Glace', 'Terre']},
+				{'name': 'Givegarde', 'level': 48, 'types': ['Glace', 'Terre']},
+				{'name': 'Wendirago', 'level': 52, 'types': ['Foudre', 'Glace']},
+				{'name': 'Chimeson', 'level': 50, 'types': ['Glace', 'Poison']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Zolt',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Tourchonnerre', 'level': 60, 'types': ['Foudre', 'Terre']},
+				{'name': 'Fulgurobuste', 'level': 64, 'types': ['Foudre', 'Terre']},
+				{'name': 'Leprennerre', 'level': 62, 'types': ['Foudre', 'Magie']},
+				{'name': 'Furfoudre', 'level': 62, 'types': ['Air', 'Foudre']},
+				{'name': 'Basamuscle', 'level': 63, 'types': ['Foudre', 'Poing']},
+				{'name': 'Pangfu', 'level': 61, 'types': ['Foudre', 'Poing']},
+				{'name': 'Elfoudre', 'level': 69, 'types': ['Foudre', 'Magie']},
+				{'name': 'Tonneresprit', 'level': 63, 'types': ['Foudre', 'Ombre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Zura',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Océchinoir', 'level': 1, 'types': ['Eau', 'Poison']},
+				{'name': 'Castorbard', 'level': 1, 'types': ['Eau', 'Métal']},
+				{'name': 'Lutalune', 'level': 1, 'types': ['Eau', 'Magie']},
+				{'name': 'Dracaille', 'level': 1, 'types': ['Eau', 'Poison']},
+				{'name': 'Ominidro', 'level': 1, 'types': ['Eau', 'Poing']},
+				{'name': 'Krakendigo', 'level': 1, 'types': ['Eau', 'Ombre']},
+				{'name': "Ô'Djinn", 'level': 1, 'types': ['Eau', 'Magie']},
+				{'name': 'Octernix', 'level': 1, 'types': ['Eau', 'Feu']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Gahia',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Cerberos', 'level': 1, 'types': ['Ombre', 'Terre']},
+				{'name': 'Gargolyric', 'level': 1, 'types': ['Air', 'Terre']},
+				{'name': 'Destriaval', 'level': 1, 'types': ['Métal', 'Terre']},
+				{'name': 'Trollice', 'level': 1, 'types': ['Magie', 'Terre']},
+				{'name': 'Cristargot', 'level': 1, 'types': ['Glace', 'Terre']},
+				{'name': 'Fulgurobuste', 'level': 1, 'types': ['Foudre', 'Terre']},
+				{'name': 'Cristemprise', 'level': 1, 'types': ['Air', 'Terre']},
+				{'name': 'Quaterrocit', 'level': 1, 'types': ['Poing', 'Terre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Jiyan',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Hippocendie', 'level': 1, 'types': ['Eau', 'Feu']},
+				{'name': 'Chevalancelot', 'level': 1, 'types': ['Feu', 'Terre']},
+				{'name': 'Ciergeorik', 'level': 1, 'types': ['Feu', 'Magie']},
+				{'name': 'Panzéros', 'level': 1, 'types': ['Feu', 'Poing']},
+				{'name': 'Transfignis', 'level': 1, 'types': ['Feu', 'Ombre']},
+				{'name': 'Luciferoce', 'level': 1, 'types': ['Feu', 'Ombre']},
+				{'name': 'Octernix', 'level': 1, 'types': ['Eau', 'Feu']},
+				{'name': 'Fourmitraille', 'level': 1, 'types': ['Feu', 'Insecte']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Astridys',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Corbachef', 'level': 1, 'types': ['Air', 'Poing']},
+				{'name': 'Harmonymne', 'level': 1, 'types': ['Air', 'Magie']},
+				{'name': 'Cristemprise', 'level': 1, 'types': ['Air', 'Terre']},
+				{'name': 'Glaviatorne', 'level': 1, 'types': ['Air', 'Métal']},
+				{'name': 'Ventisinge', 'level': 1, 'types': ['Air', 'Poing']},
+				{'name': 'Gargolyric', 'level': 1, 'types': ['Air', 'Terre']},
+				{'name': 'Renfield', 'level': 1, 'types': ['Air', 'Ombre']},
+				{'name': 'Licère', 'level': 1, 'types': ['Air', 'Feu']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Kior',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Cerberos', 'level': 1, 'types': ['Ombre', 'Terre']},
+				{'name': 'Cavafroi', 'level': 1, 'types': ['Métal', 'Ombre']},
+				{'name': 'Licheroi', 'level': 1, 'types': ['Métal', 'Ombre']},
+				{'name': 'Cafarnaöm', 'level': 1, 'types': ['Insecte', 'Ombre']},
+				{'name': 'Obsidarque', 'level': 1, 'types': ['Magie', 'Ombre']},
+				{'name': 'Scolombre', 'level': 1, 'types': ['Insecte', 'Ombre']},
+				{'name': 'Luciferoce', 'level': 1, 'types': ['Feu', 'Ombre']},
+				{'name': 'Kangbaston', 'level': 1, 'types': ['Ombre', 'Poing']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Wladim',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Dratasque', 'level': 1, 'types': ['Dragon', 'Terre']},
+				{'name': 'Vouizzard', 'level': 1, 'types': ['Dragon', 'Glace']},
+				{'name': 'Céledrac', 'level': 1, 'types': ['Air', 'Dragon']},
+				{'name': 'Nécrogon', 'level': 1, 'types': ['Dragon', 'Ombre']},
+				{'name': 'Fulgyran', 'level': 1, 'types': ['Dragon', 'Foudre']},
+				{'name': 'Pandragon', 'level': 1, 'types': ['Dragon', 'Poing']},
+				{'name': 'Sabragon', 'level': 1, 'types': ['Dragon', 'Métal']},
+				{'name': 'Dragolance', 'level': 1, 'types': ['Dragon', 'Métal']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Azami',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Magmamba', 'level': 1, 'types': ['Feu', 'Poison']},
+				{'name': 'Venopine', 'level': 1, 'types': ['Poison', 'Terre']},
+				{'name': 'Viverpine', 'level': 1, 'types': ['Poison', 'Terre']},
+				{'name': 'Dracaille', 'level': 1, 'types': ['Eau', 'Poison']},
+				{'name': 'Armuropion', 'level': 1, 'types': ['Métal', 'Poison']},
+				{'name': 'Reynorage', 'level': 1, 'types': ['Foudre', 'Insecte']},
+				{'name': 'Chimeson', 'level': 1, 'types': ['Glace', 'Poison']},
+				{'name': 'Cryobra', 'level': 1, 'types': ['Glace', 'Poison']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Talaric',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Tarensabre', 'level': 1, 'types': ['Métal', 'Poison']},
+				{'name': 'Destriaval', 'level': 1, 'types': ['Métal', 'Terre']},
+				{'name': 'Glaviatorne', 'level': 1, 'types': ['Air', 'Métal']},
+				{'name': 'Armuropion', 'level': 1, 'types': ['Métal', 'Poison']},
+				{'name': 'Sabragon', 'level': 1, 'types': ['Dragon', 'Métal']},
+				{'name': 'Hyperlem', 'level': 1, 'types': ['Feu', 'Métal']},
+				{'name': 'Frelord', 'level': 1, 'types': ['Insecte', 'Métal']},
+				{'name': 'Licheroi', 'level': 1, 'types': ['Métal', 'Ombre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Marion',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Corbachef', 'level': 1, 'types': ['Air', 'Poing']},
+				{'name': 'Chevareuil', 'level': 1, 'types': ['Métal', 'Poing']},
+				{'name': 'Panzéros', 'level': 1, 'types': ['Feu', 'Poing']},
+				{'name': 'Basamuscle', 'level': 1, 'types': ['Foudre', 'Poing']},
+				{'name': 'Pangfu', 'level': 1, 'types': ['Foudre', 'Poing']},
+				{'name': 'Kangbaston', 'level': 1, 'types': ['Ombre', 'Poing']},
+				{'name': 'Guêpitaine', 'level': 1, 'types': ['Insecte', 'Poing']},
+				{'name': 'Pyromate', 'level': 1, 'types': ['Feu', 'Poing']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Archimëlle',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Ciergeorik', 'level': 1, 'types': ['Feu', 'Magie']},
+				{'name': 'Obsidarque', 'level': 1, 'types': ['Magie', 'Ombre']},
+				{'name': 'Leprennerre', 'level': 1, 'types': ['Foudre', 'Magie']},
+				{'name': "Ô'Djinn", 'level': 1, 'types': ['Eau', 'Magie']},
+				{'name': 'Elfoudre', 'level': 1, 'types': ['Foudre', 'Magie']},
+				{'name': 'Trollice', 'level': 1, 'types': ['Magie', 'Terre']},
+				{'name': 'Lutalune', 'level': 1, 'types': ['Eau', 'Magie']},
+				{'name': 'Matermage', 'level': 1, 'types': ['Magie', 'Terre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Sylviae',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Reynorage', 'level': 1, 'types': ['Foudre', 'Insecte']},
+				{'name': 'Scarazzard', 'level': 1, 'types': ['Glace', 'Insecte']},
+				{'name': 'Fourmitraille', 'level': 1, 'types': ['Feu', 'Insecte']},
+				{'name': 'Cafarnaöm', 'level': 1, 'types': ['Insecte', 'Ombre']},
+				{'name': 'Eollule', 'level': 1, 'types': ['Air', 'Insecte']},
+				{'name': 'Sautoxy', 'level': 1, 'types': ['Insecte', 'Poison']},
+				{'name': 'Mystillon', 'level': 1, 'types': ['Insecte', 'Magie']},
+				{'name': 'Mouclair', 'level': 1, 'types': ['Foudre', 'Insecte']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Nigel',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Velido', 'level': 1, 'types': ['Eau']},
+				{'name': 'Castorbard', 'level': 1, 'types': ['Eau', 'Métal']},
+				{'name': 'Lutalune', 'level': 1, 'types': ['Eau', 'Magie']},
+				{'name': 'Dracaille', 'level': 1, 'types': ['Eau', 'Poison']},
+				{'name': 'Hippocendie', 'level': 1, 'types': ['Eau', 'Feu']},
+				{'name': 'Krakendigo', 'level': 1, 'types': ['Eau', 'Ombre']},
+				{'name': 'Léviathan', 'level': 1, 'types': ['Eau']},
+				{'name': 'Armuropion', 'level': 1, 'types': ['Métal', 'Poison']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Galathée',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Scorpidard', 'level': 1, 'types': ['Poison', 'Terre']},
+				{'name': 'Serpenigre', 'level': 1, 'types': ['Ombre', 'Poison']},
+				{'name': 'Viverpine', 'level': 1, 'types': ['Poison', 'Terre']},
+				{'name': 'Saterra', 'level': 1, 'types': ['Terre']},
+				{'name': 'Matermage', 'level': 1, 'types': ['Magie', 'Terre']},
+				{'name': 'Trollice', 'level': 1, 'types': ['Magie', 'Terre']},
+				{'name': 'Tarensabre', 'level': 1, 'types': ['Métal', 'Poison']},
+				{'name': 'Venopine', 'level': 1, 'types': ['Poison', 'Terre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Follet',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Transfignis', 'level': 1, 'types': ['Feu', 'Ombre']},
+				{'name': 'Luciferoce', 'level': 1, 'types': ['Feu', 'Ombre']},
+				{'name': 'Gévauloup', 'level': 1, 'types': ['Feu', 'Métal']},
+				{'name': 'Phénix', 'level': 1, 'types': ['Feu']},
+				{'name': 'Scolombre', 'level': 1, 'types': ['Insecte', 'Ombre']},
+				{'name': 'Werdoba', 'level': 1, 'types': ['Feu', 'Glace']},
+				{'name': 'Panzéros', 'level': 1, 'types': ['Feu', 'Poing']},
+				{'name': 'Ciergeorik', 'level': 1, 'types': ['Feu', 'Magie']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Julia',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Ciergeorik', 'level': 1, 'types': ['Feu', 'Magie']},
+				{'name': 'Vampyroi', 'level': 1, 'types': ['Air', 'Poison']},
+				{'name': 'Zéphyr', 'level': 1, 'types': ['Air']},
+				{'name': 'Corbachef', 'level': 1, 'types': ['Air', 'Poing']},
+				{'name': 'Harmonymne', 'level': 1, 'types': ['Air', 'Magie']},
+				{'name': 'Glaviatorne', 'level': 1, 'types': ['Air', 'Métal']},
+				{'name': 'Ventisinge', 'level': 1, 'types': ['Air', 'Poing']},
+				{'name': 'Eollule', 'level': 1, 'types': ['Air', 'Insecte']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Kaïla',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Tourchonnerre', 'level': 1, 'types': ['Foudre', 'Terre']},
+				{'name': 'Fulgurobuste', 'level': 1, 'types': ['Foudre', 'Terre']},
+				{'name': 'Reynorage', 'level': 1, 'types': ['Foudre', 'Insecte']},
+				{'name': 'Leprennerre', 'level': 1, 'types': ['Foudre', 'Magie']},
+				{'name': 'Furfoudre', 'level': 1, 'types': ['Air', 'Foudre']},
+				{'name': 'Basamuscle', 'level': 1, 'types': ['Foudre', 'Poing']},
+				{'name': 'Pangfu', 'level': 1, 'types': ['Foudre', 'Poing']},
+				{'name': 'Guêpitaine', 'level': 1, 'types': ['Insecte', 'Poing']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Ramëlia',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Cafarnaöm', 'level': 1, 'types': ['Insecte', 'Ombre']},
+				{'name': 'Sautoxy', 'level': 1, 'types': ['Insecte', 'Poison']},
+				{'name': 'Frelord', 'level': 1, 'types': ['Insecte', 'Métal']},
+				{'name': 'Mouclair', 'level': 1, 'types': ['Foudre', 'Insecte']},
+				{'name': 'Nantoile', 'level': 1, 'types': ['Insecte', 'Néant']},
+				{'name': 'Arkhan', 'level': 1, 'types': ['Magie', 'Néant']},
+				{'name': 'Toxide', 'level': 1, 'types': ['Néant', 'Poison']},
+				{'name': 'Mystôme', 'level': 1, 'types': ['Néant', 'Ombre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Shawn',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Dratasque', 'level': 1, 'types': ['Dragon', 'Terre']},
+				{'name': 'Fulgyran', 'level': 1, 'types': ['Dragon', 'Foudre']},
+				{'name': 'Virugon', 'level': 1, 'types': ['Dragon', 'Poison']},
+				{'name': 'Vouizzard', 'level': 1, 'types': ['Dragon', 'Glace']},
+				{'name': 'Oursberg', 'level': 1, 'types': ['Eau', 'Glace']},
+				{'name': 'Werdoba', 'level': 1, 'types': ['Feu', 'Glace']},
+				{'name': 'Wendirago', 'level': 1, 'types': ['Foudre', 'Glace']},
+				{'name': 'Givegarde', 'level': 1, 'types': ['Glace', 'Terre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Raël',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Fantagon', 'level': 1, 'types': ['Dragon', 'Magie']},
+				{'name': 'Vouizzard', 'level': 1, 'types': ['Dragon', 'Glace']},
+				{'name': 'Fulgyran', 'level': 1, 'types': ['Dragon', 'Foudre']},
+				{'name': 'Nécrogon', 'level': 1, 'types': ['Dragon', 'Ombre']},
+				{'name': 'Virugon', 'level': 1, 'types': ['Dragon', 'Poison']},
+				{'name': 'Pandragon', 'level': 1, 'types': ['Dragon', 'Poing']},
+				{'name': 'Sabragon', 'level': 1, 'types': ['Dragon', 'Métal']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Archéon',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Hyperlem', 'level': 100, 'types': ['Feu', 'Métal']},
+				{'name': 'Glaviatorne', 'level': 100, 'types': ['Air', 'Métal']},
+				{'name': 'Dragolance', 'level': 100, 'types': ['Dragon', 'Métal']},
+				{'name': 'Destriaval', 'level': 100, 'types': ['Métal', 'Terre']},
+				{'name': 'Tarensabre', 'level': 100, 'types': ['Métal', 'Poison']},
+				{'name': 'Cavafroi', 'level': 100, 'types': ['Métal', 'Ombre']},
+				{'name': 'Licheroi', 'level': 100, 'types': ['Métal', 'Ombre']},
+				{'name': 'Castorbard', 'level': 100, 'types': ['Eau', 'Métal']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Chaos',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Toxide', 'level': 1, 'types': ['Néant', 'Poison']},
+				{'name': 'Frosthral', 'level': 1, 'types': ['Glace', 'Néant']},
+				{'name': 'Closmach', 'level': 1, 'types': ['Néant', 'Poing']},
+				{'name': 'Mystôme', 'level': 1, 'types': ['Néant', 'Ombre']},
+				{'name': 'Aetheryx', 'level': 1, 'types': ['Dragon', 'Néant']},
+				{'name': 'Néon', 'level': 1, 'types': ['Foudre', 'Néant']},
+				{'name': 'Arkhan', 'level': 1, 'types': ['Magie', 'Néant']},
+				{'name': 'Chaotic', 'level': 1, 'types': ['Métal', 'Néant']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Janus',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Aetheryx', 'level': 1, 'types': ['Dragon', 'Néant']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Februus',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Mystôme', 'level': 1, 'types': ['Néant', 'Ombre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Mars',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Closmach', 'level': 1, 'types': ['Néant', 'Poing']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Aphrodite',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Toxide', 'level': 1, 'types': ['Néant', 'Poison']},
+				{'name': 'Sifflana', 'level': 1, 'types': ['Eau', 'Poison']},
+				{'name': 'Tarensabre', 'level': 1, 'types': ['Métal', 'Poison']},
+				{'name': 'Océchinoir', 'level': 1, 'types': ['Eau', 'Poison']},
+				{'name': 'Serpenigre', 'level': 1, 'types': ['Ombre', 'Poison']},
+				{'name': 'Venopine', 'level': 1, 'types': ['Poison', 'Terre']},
+				{'name': 'Dracaille', 'level': 1, 'types': ['Eau', 'Poison']},
+				{'name': 'Vampyroi', 'level': 1, 'types': ['Air', 'Poison']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Maia',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Géonarque', 'level': 1, 'types': ['Néant', 'Terre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Junon',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Arkhan', 'level': 1, 'types': ['Magie', 'Néant']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Jupiter',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Néon', 'level': 1, 'types': ['Foudre', 'Néant']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Apollon',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Vaporol', 'level': 1, 'types': ['Air', 'Néant']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Saturne',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Chaotic', 'level': 1, 'types': ['Métal', 'Néant']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Octé',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Frosthral', 'level': 1, 'types': ['Glace', 'Néant']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Neptune',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Néptuléen', 'level': 1, 'types': ['Eau', 'Néant']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Diana',
+	teams: [
+		{
+			name: 'Equipe',
+			admons: [
+				{'name': 'Phlogiston', 'level': 1, 'types': ['Feu', 'Néant']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Sbire1',
+	teams: [
+		{
+			name: 'Team1',
+			admons: [
+				{'name': 'Minilem', 'level': 1, 'types': ['Feu', 'Métal']},
+				{'name': 'Canirdoba', 'level': 1, 'types': ['Feu', 'Glace']},
+				{'name': 'Cavataille', 'level': 1, 'types': ['Foudre', 'Ombre']},
+				{'name': 'Tamiclier', 'level': 1, 'types': ['Métal']},
+				{'name': 'Araignard', 'level': 1, 'types': ['Métal', 'Poison']},
+				{'name': 'Ecuyeur', 'level': 1, 'types': ['Métal', 'Ombre']},
+				{'name': 'Papyruth', 'level': 1, 'types': ['Ombre']},
+				{'name': 'Burroque', 'level': 1, 'types': ['Terre']}
+			]
+		},
+		{
+			name: 'Team2',
+			admons: [
+				{'name': 'Minilem', 'level': 1, 'types': ['Feu', 'Métal']},
+				{'name': 'Cavataille', 'level': 1, 'types': ['Foudre', 'Ombre']},
+				{'name': 'Tamiclier', 'level': 1, 'types': ['Métal']},
+				{'name': 'Araignard', 'level': 1, 'types': ['Métal', 'Poison']},
+				{'name': 'Papyruth', 'level': 1, 'types': ['Ombre']},
+				{'name': 'Burroque', 'level': 1, 'types': ['Terre']},
+				{'name': 'Pincelancier', 'level': 1, 'types': ['Poison']},
+				{'name': 'Canirdoba', 'level': 1, 'types': ['Feu', 'Glace']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Sbire2',
+	teams: [
+		{
+			name: 'Team1',
+			admons: [
+				{'name': 'Minilem', 'level': 1, 'types': ['Feu', 'Métal']},
+				{'name': 'Ragoncier', 'level': 1, 'types': ['Eau', 'Métal']},
+				{'name': 'Tamiclier', 'level': 1, 'types': ['Métal']},
+				{'name': 'Noctolame', 'level': 1, 'types': ['Air', 'Métal']},
+				{'name': 'Pincelancier', 'level': 1, 'types': ['Poison']},
+				{'name': 'Ecuyeur', 'level': 1, 'types': ['Métal', 'Ombre']},
+				{'name': 'Papyruth', 'level': 1, 'types': ['Ombre']},
+				{'name': 'Araignard', 'level': 1, 'types': ['Métal', 'Poison']}
+			]
+		},
+		{
+			name: 'Team2',
+			admons: [
+				{'name': 'Minilem', 'level': 1, 'types': ['Feu', 'Métal']},
+				{'name': 'Canirdoba', 'level': 1, 'types': ['Feu', 'Glace']},
+				{'name': 'Ragoncier', 'level': 1, 'types': ['Eau', 'Métal']},
+				{'name': 'Tamiclier', 'level': 1, 'types': ['Métal']},
+				{'name': 'Araignard', 'level': 1, 'types': ['Métal', 'Poison']},
+				{'name': 'Noctolame', 'level': 1, 'types': ['Air', 'Métal']},
+				{'name': 'Frelord', 'level': 1, 'types': ['Insecte', 'Métal']},
+				{'name': 'Papyruth', 'level': 1, 'types': ['Ombre']}
+			]
+		}
+	]
+},
+{
+	dresseur: 'Orionis',
+	teams: [
+		{
+			name: 'Team1',
+			admons: [
+				{'name': 'Obsidarque', 'level': 1, 'types': ['Magie', 'Ombre']}
+			]
+		}
+	]
+}];
